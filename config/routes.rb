2dc174d884ba38c47fc706d50115b42fc133d57b
rajmohitrtaskcraft.in <rajmohit.r@taskcraft.in>
2023-01-31 17:54:33 +0530

    second

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users do
+   delete '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  root "messages#index"
+  resources :messages do 
+    delete  '/messages/:id' => 'messages#destroy'
+    resources :comments
+    
+  end
 end
---
+++ db/migrate/20230131054225_add_user_to_messages.rb
@@ -0,0 +1,5 @@
+class AddUserToMessages < ActiveRecord::Migration[7.0]
+  def change
+    add_column :messages, :user_id, :integer
+  end
+end
